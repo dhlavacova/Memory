774ce8f4eca09fb6bd175ab75bc24684fa2c3bf4
Dana <dana@hlavacova.de>
2023-05-14 17:17:29 +0200

    zufallige bilder

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,37 @@ import { useState,useEffect} from 'react'
 import './App.css'
 import Memory from './components/Memory.jsx'
 function App() {
-    const[data,setData]=useState([]);
+    const[data,setData]=useState(()=>{
+        const localData=localStorage.getItem("memory")
+        return localData?JSON.parse(localData):[]
+    });
   const  fetchData=async()=>{
         try {
-            const data = await fetch("https://rickandmortyapi.com/api/character/")
-            const dataJson = await data.json()
-            const results = dataJson.results
+            const dataFetch = await fetch("https://rickandmortyapi.com/api/character/")
+            const dataJson = await dataFetch.json()
+            const results = dataJson.results.slice(0,6)
             setData(results)
+            console.log(results)
         }
         catch (error){
             console.log('chyba pri nacteni dat:',error)
         }
     }
     useEffect(()=>{
-        fetchData()
+
+         if(data.length===0) {
+             fetchData();
+         }
+else {
+             localStorage.setItem("memory", JSON.stringify(data))
+         }
     },[])
 
-
-  return (
+       return (
     <>
         <h1>Memory</h1>
-        <ul>
-            {data.map(item => (
-                <li key={item.id}>{item.name}</li>
-            ))}
-        </ul>
-      <Memory/>
+
+       <Memory data={data}/>
 
     </>
   )
--- src/components/Memory.jsx
+++ src/components/Memory.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './memory.scss'
 
-const Memory = () => {
+const Memory = ({data}) => {
+    const karten = [...data, ...data]
+
+    for (let i = karten.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = karten[i];
+        karten[i] = karten[j];
+        karten[j] = temp;
+    }
+
     return (
         <div className="container">
 
+            {karten.map(el => <img key={el.id} src={el.image} alt={el.name}/>)
+            }
         </div>
     );
 };
