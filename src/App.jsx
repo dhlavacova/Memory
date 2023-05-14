ca41a6d30c7650309e2a884739cb022f1fe23c1f
Dana <dana@hlavacova.de>
2023-05-14 09:22:27 +0200

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,31 @@ import { useState,useEffect} from 'react'
 import './App.css'
 import Memory from './components/Memory.jsx'
 function App() {
-  const  collApi=async()=>{
-      const data=await fetch("https://rickandmortyapi.com/api/character/")
-      const dataJson=await data.json()
-      console.log(JSON.stringify(dataJson))
+    const[data,setData]=useState([]);
+  const  fetchData=async()=>{
+        try {
+            const data = await fetch("https://rickandmortyapi.com/api/character/")
+            const dataJson = await data.json()
+            const results = dataJson.results
+            setData(results)
+        }
+        catch (error){
+            console.log('chyba pri nacteni dat:',error)
+        }
     }
     useEffect(()=>{
-        if(local.sto){}
-            collApi()})
-//collApi()
+        fetchData()
+    },[])
+
+
   return (
     <>
-
+        <h1>Memory</h1>
+        <ul>
+            {data.map(item => (
+                <li key={item.id}>{item.name}</li>
+            ))}
+        </ul>
       <Memory/>
 
     </>
